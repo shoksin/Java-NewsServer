25c37255f6a94e368041f05db7e8cab940b5b9c8
Artsiom Naumchyk <anaumcik300@gmail.com>
2024-11-17 15:41:17 +0300

    main logic

---
+++ src/Client.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        try(Socket socket = new Socket("localhost", 3345);
+            BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+            DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
+            DataInputStream ois = new DataInputStream(socket.getInputStream()); )
+        {
+
+            System.out.println("Client connected to socket.");
+            System.out.println();
+            System.out.println("Client writing channel = oos & reading channel = ois initialized.");
+            System.out.println("Command:\ndd.mm.yy");
+
+            while(!socket.isOutputShutdown()){
+
+                if(br.ready()){
+
+                    System.out.println("Client start writing in channel...");
+                    Thread.sleep(1000);
+                    String clientCommand = br.readLine();
+
+                    oos.writeUTF(clientCommand);
+                    oos.flush();
+                    System.out.println("Message " + clientCommand + " sended to server.");
+                    Thread.sleep(1000);
+
+                    if(clientCommand.equalsIgnoreCase("quit")){
+
+                        System.out.println("Connection killed");
+                        Thread.sleep(2000);
+
+                        if(ois.read() > -1)     {
+                            System.out.println("reading...");
+                            String in = ois.readUTF();
+                            System.out.println(in);
+                        }
+
+                        break;
+                    }
+
+                    System.out.println("start waiting for data from server...");
+                    Thread.sleep(2000);
+
+                    if(ois.read() > -1)     {
+
+                        System.out.println("reading...");
+                        String in = ois.readUTF();
+                        System.out.println(in);
+                    }
+                }
+            }
+            System.out.println("Connection closed.");
+
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host.");
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Failed to connect to the socket");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    static ExecutorService executeIt = Executors.newFixedThreadPool(4);
+
+    public static void main(String[] args) {
+
+        // стартуем сервер на порту 3345 и инициализируем переменную для обработки консольных команд с самого сервера
+        try (ServerSocket server = new ServerSocket(3345);
+             BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println("Server socket created, command console reader for listen to server commands");
+
+            while (!server.isClosed()) {
+
+                if (br.ready()) {
+                    System.out.println("Main Server found any messages in channel, let's look at them.");
+
+                    // если команда - quit то инициализируем закрытие сервера и
+                    // выход из цикла раздачии нитей монопоточных серверов
+                    String serverCommand = br.readLine();
+                    if (serverCommand.equalsIgnoreCase("quit")) {
+                        System.out.println("Main Server initiate exiting...");
+                        server.close();
+                        break;
+                    }
+                }
+
+                // если комманд от сервера нет то становимся в ожидание
+                // подключения к сокету общения под именем - "clientDialog" на
+                // серверной стороне
+                System.out.println("Server socket listening on port 3345");
+                Socket client = server.accept();
+                System.out.println("Client connected");
+
+                // после получения запроса на подключение сервер создаёт сокет
+                // для общения с клиентом и отправляет его в отдельную нить
+                // в Runnable(при необходимости можно создать Callable)
+                // монопоточную нить = сервер - MonoThreadHandler и тот
+                // продолжает общение от лица сервера
+                executeIt.execute(new MonoThreadHandler(client));
+                System.out.println("Connection accepted.");
+            }
+
+            // закрытие пула нитей после завершения работы всех нитей
+            executeIt.shutdown();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
